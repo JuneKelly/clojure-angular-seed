f2a41b34d21f245175402eb5694892c0e405988b
Shane Kilkelly <shane@kilkelly.me>
2014-01-16 18:50:01 +0000

    update libraries

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [lib-noir "0.7.6"]
+                 [lib-noir "0.7.9"]
                  [compojure "1.1.6"]
                  [ring-server "0.3.1"]
-                 [selmer "0.5.4"]
+                 [selmer "0.5.8"]
                  [com.taoensso/timbre "2.7.1"]
                  [com.postspectacular/rotor "0.1.0"]
                  [com.taoensso/tower "2.0.1"]
-                 [markdown-clj "0.9.35"]
+                 [markdown-clj "0.9.40"]
                  [liberator "0.10.0"]
                  [clj-time "0.6.0"]
                  [environ "0.4.0"]]
